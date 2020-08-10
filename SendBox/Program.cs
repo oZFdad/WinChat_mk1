e66752178a6017c1d03bc4164ba57205efcf0eb0
oZF <ozf_dad@mail.ru>
2020-08-10 13:57:16 +0500

    Add SendBox

---
+++ SendBox/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace SendBox
+{
+    class Program
+    {
+        static AutoResetEvent waitHandler = new AutoResetEvent(true);
+        static int x = 0;
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread myThread = new Thread(Count);
+                myThread.Name = $"Поток {i.ToString()}";
+                myThread.Start();
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void Count()
+        {
+            waitHandler.WaitOne();
+            x = 1;
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+                x++;
+                Thread.Sleep(100);
+            }
+
+            waitHandler.Set();
+        }
+    }
+}
